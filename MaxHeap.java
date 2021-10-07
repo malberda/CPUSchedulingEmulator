fab6062eebbddef5385206a0d1a7405b1507e987
malberda <malberda15@gmail.com>
2021-10-07 12:46:21 -0600

    added maxheapify and unfinished heapcomparison function

--- MaxHeap.java
+++ MaxHeap.java
@@ -39,7 +39,29 @@ public class MaxHeap
 
 	private void maxHeapify(int position)
 	{
+		if(isleaf(position))
+		{
+			return;
+		}
+		if(heapComparison(Heap[position],Heap[leftChildPosition(position)])<0 || 
+		   heapComparison(Heap[position],Heap[rightChildPosition(position)])<0)
+		{
+			if(heapComparison(Heap[leftChildPosition(position)],Heap[rightChildPosition(position)])>0)
+			{
+				swap(position,leftChildPosition(position));
+				maxHeapify(leftChildPosition(position));
+			}
+			else
+			{
+				swap(position,rightChildPosition(position));
+				maxHeapify(rightChildPosition(position));
+			}
+		}
+	}
 
+	private int heapComparison(Heap one, Heap two)
+	{
+		return 1;//need to fix this
 	}
 
 
