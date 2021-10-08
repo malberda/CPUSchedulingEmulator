7a9aab4436578a88c3f58eefcdd1d5f5c13c7ed0
malberda <malberda15@gmail.com>
2021-10-07 22:15:18 -0600

    finished Process.java probably. added everything but reduceTimeRemaining, because i dont know how much to reduce it by

--- Process.java
+++ Process.java
@@ -1,15 +1,61 @@
 public class Process
 {
 	private int priority;
+	private int timeRemaining;
 	private int arrivalTime;
-	private int cpuBurstTime;
+	private int timeNotProcessed;
 
 	public Process(int a, int b, int c)
 	{
 		this.priority=a;
-		this.arrivalTime=b;
-		this.cpuBurstTime=c;
+		this.timeRemaining=b;
+		this.arrivalTime=c;
+		this.timeNotProcessed=0;
 	}
+
+	public int compareTo(int a, int b)
+	{
+		if (a<b)
+			return -1;
+		else if (a>b)
+			return 1;
+		else
+			return 0;
+	}
+
+	public void reduceTimeRemaining()
+	{
+
+	}
+
+	public boolean finish()
+	{
+		if(this.timeRemaining<=0)
+			return true;
+		else
+			return false;
+	}
+
+	public int getArrivalTime()
+	{
+		return this.arrivalTime;
+	}
+
+	public int getPriority()
+	{
+		return this.priority;
+	}
+
+	public int getTimeRemaining()
+	{
+		return this.timeRemaining;
+	}
+
+	public void resetTimeNotProcessed()
+	{
+		this.timeNotProcessed=0;
+	}
+
 
 
 }
