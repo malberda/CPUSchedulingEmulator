eb02e6ae53c9c0075764ddf84c91f8f57f38bfe7
malberda <malberda15@gmail.com>
2021-10-07 19:38:52 -0600

    added constructor to process

--- Process.java
+++ Process.java
@@ -1,6 +1,15 @@
 public class Process
 {
+	private int priority;
+	private int arrivalTime;
+	private int cpuBurstTime;
 
+	public Process(int a, int b, int c)
+	{
+		this.priority=a;
+		this.arrivalTime=b;
+		this.cpuBurstTime=c;
+	}
 
 
 }
